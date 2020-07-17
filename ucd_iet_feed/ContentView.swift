26774bb7a8702629a639e6633fbaa769f3c86bfd
Tyler Jachetta <jachetta.tyler@gmail.com>
2020-07-16 19:42:46 -0700

    Initial Commit

---
+++ ucd_iet_feed/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ucd_iet_feed
+//
+//  Created by Tyler Jachetta on 7/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ucd_iet_feed/ucd_iet_feedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ucd_iet_feedApp.swift
+//  ucd_iet_feed
+//
+//  Created by Tyler Jachetta on 7/16/20.
+//
+
+import SwiftUI
+
+@main
+struct ucd_iet_feedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
